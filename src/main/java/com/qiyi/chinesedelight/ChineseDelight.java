05eab49d766234ff19dcb6984404cad726208120
MinorEevee <a56850809@163.com>
2026-09-25 15:09:22 +0800

    0.1.1b feat:添加多种菜品

--- src/main/java/com/qiyi/chinesedelight/ChineseDelight.java
+++ src/main/java/com/qiyi/chinesedelight/ChineseDelight.java
@@ -6,12 +6,15 @@ import com.mojang.logging.LogUtils;
 
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import com.qiyi.chinesedelight.attachment.ModAttachments;
 import com.qiyi.chinesedelight.block.ModBlockEntities;
 import com.qiyi.chinesedelight.block.ModBlocks;
+import com.qiyi.chinesedelight.client.HudConfig;
 import com.qiyi.chinesedelight.cooking.ModCookingRegistries;
 import com.qiyi.chinesedelight.entity.ModEntities;
 import com.qiyi.chinesedelight.item.ModCreativeTabs;
@@ -29,7 +32,7 @@ public class ChineseDelight
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public ChineseDelight(IEventBus modEventBus)
+    public ChineseDelight(IEventBus modEventBus, ModContainer modContainer)
     {
         // Register the Deferred Registers of every registry this mod adds entries to
         ModItems.register(modEventBus);
@@ -40,6 +43,9 @@ public class ChineseDelight
         ModCookingRegistries.register(modEventBus);
         ModAttachments.register(modEventBus);
         ModNetwork.register(modEventBus);
+
+        // 客户端设置：厨师帽抬头显示的缩放 / 位置 / 透明度，只影响本机表现
+        modContainer.registerConfig(ModConfig.Type.CLIENT, HudConfig.SPEC);
 
         NeoForge.EVENT_BUS.register(this);
 
